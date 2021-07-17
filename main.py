ab85a02bb7f0152c9835d03f34847f1fe00d1843
Amy_hui <56364401+Amy-1114@users.noreply.github.com>
2021-07-17 12:35:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+books = ['a','b','c','d']
+print(books[-1].title())
+books[-1]='g'
+print(books)
+books.append('h')
+print(books)
+books.insert(1,'k')
+print(books)
+books.remove('b')
+print(books)
